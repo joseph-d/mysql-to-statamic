ea6e56e9c2758db677ccddfcbfabc7326b2b09bd
Joseph D <joseph.d@nirada.co.th>
2021-03-26 15:10:23 +0000

    updated to yaml

--- src/Console/DatabaseToStatamic.php
+++ src/Console/DatabaseToStatamic.php
@@ -4,10 +4,15 @@ namespace Josephd\MysqlToStatamic\Console;
 
 use Illuminate\Console\Command;
 use Statamic\Facades\Collection;
+use Statamic\Facades\Taxonomy;
+use Statamic\Facades\GlobalSet;
 use Statamic\Facades\Entry;
+use Statamic\Facades\Term;
 use Statamic\Facades\Stache;
 use Illuminate\Support\Facades\DB;
 use Carbon\Carbon;
+use Symfony\Component\Yaml\Yaml;
+use Illuminate\Support\Facades\Storage;
 
 class DatabaseToStatamic extends Command
 {
@@ -17,11 +22,7 @@ class DatabaseToStatamic extends Command
      * @var string
      */
     protected $signature = 'mysql-to-statamic:run
-                            {--table= : MySQL table name}
-                            {--field=* : Optional field names required}
-                            {--collection= : Statamic collection handle}
-                            {--imagePrefix= : Optional image URL prefix to be replaced with asset tag}
-                            {--assetTag= : Optional asset tag to replace image URL prefix}
+                            {--conversion= : Name of conversion to run}
                             { --confirm= : Run without showing confirmation }';
 
     /**
@@ -29,7 +30,7 @@ class DatabaseToStatamic extends Command
      *
      * @var string
      */
-    protected $description = 'Converts data from Laravel-style MySQL databases to Statamic YAML';
+    protected $description = 'Converts data from MySQL databases to Statamic YAML';
 
     /**
      * Create a new command instance.
@@ -48,162 +49,108 @@ class DatabaseToStatamic extends Command
      */
     public function handle()
     {
-        $this->newLine();
-
-        if (!Collection::handles()->all()) {
-            $this->error('😲 No Statamic collections found. Aborting.');
-            $this->newLine();
-            exit();
-        } else {
-            $this->warn('❗USE WITH CAUTION❗ Back up existing Statamic collections before proceeding!');
-            $this->newLine();
+        if (!$conversion = $this->option('conversion')) {
+            $conversion = $this->choice('Which conversion do you want to run? 🤔', array_flatten(array_diff(scandir(config_path('conversions/')), array('..', '.'))));
         }
-
-        if (!$tableName = $this->option('table')) {
-            $tablePrefix = $this->ask('Please enter a table prefix (or leave blank for none)');
-
-            $tableList = $this->getTableList($tablePrefix);
-
-            $tableName = $this->choice('Which table do you want to convert? 🤔',$tableList);
-        }
-
-        if (!$fieldNames = $this->option('field')) {
-            $fieldList = $this->getFieldList($tableName);
         
-            $fieldNames = $this->choice(
-                'Which optional field(s) do you want to convert? 🤔 (seperated with commas)',
-                $fieldList,
-                null,
-                $maxAttempts = null,
-                $allowMultipleSelections = true
-            );
-        }
+        $this->yaml = Yaml::parse(file_get_contents(config_path('conversions/' . $conversion)));
 
-        if (!$collectionName = $this->option('collection')) {
-            $collectionName = $this->choice('Please choose a Statamic collection name', Collection::handles()->all());
-        }
+        $this->tableName = $this->yaml['table']['name'];
+        $this->tableLimit = $this->yaml['table']['limit'] ?? null;
+        $this->date = $this->yaml['table']['date'] ?? null;
+        $this->fieldNames = $this->yaml['table']['fields'];
+        $this->destinationType = $this->yaml['destination']['type'];
+        $this->destinationName = $this->yaml['destination']['collectionName'];
 
-        if (!$imagePrefix = $this->option('imagePrefix')) {
-            $imagePrefix = $this->ask('If you need to replace an image URL prefix with a Statamic asset tag, enter the URL prefix here (optional)');
-        }
+        $this->replicatorName = $this->yaml['destination']['replicatorName'] ?? null;
+        $this->imagePrefix = $this->yaml['imageLinks']['prefix'] ?? null;
+        $this->assetTag = $this->yaml['imageLinks']['assetTag'] ?? null;
+        
+        $entries = $this->getEntries();
 
-        if ($imagePrefix && !$assetTag = $this->option('assetTag')) {
-            $assetTag = $this->ask('Please give the corresponding asset tag for ' . $imagePrefix, 'asset::assets::');
-        } else {
-            $assetTag = $this->option('assetTag');
-        }
-
-        $entries = DB::table($tableName)->get();
-
-        $this->info(count($fieldNames) . ' fields across ' . count($entries) . ' records will be converted');
+        $this->info(count($this->fieldNames) . ' fields across ' . count($entries) . ' records will be converted');
 
         $this->newLine();
 
-        if ($imagePrefix && $assetTag) {
-            $this->info('All occurances of \'' . $imagePrefix . '\' will be replaced with \'' . $assetTag . '\'');
+        if ($this->imagePrefix && $this->assetTag) {
+            $this->info('All occurances of \'' . $this->imagePrefix . '\' will be replaced with \'' . $this->assetTag . '\'');
         }
 
 
         if ($this->option('confirm') || $this->confirm('Are you ready? 🏁', false)) {
+            $this->info('Converting ' . $this->tableName . " 🚧");
             
-            $this->info('Converting ' . $tableName . " 🚧");
-            
-            $this->convertRecords($entries, $fieldNames, $collectionName, $imagePrefix, $assetTag);
+            $this->convertRecords($entries, $this->fieldNames, $this->destinationName, $this->imagePrefix, $this->assetTag, $this->destinationType);
         
-            $this->info('The ' . $tableName . ' MySQL table has been converted into Statamic YAML collection \''. $collectionName . '\' ✨😀');
+            $this->info('The ' . $this->tableName . ' MySQL table has been converted into Statamic ' . $this->destinationType . ' \''. $this->destinationName . '\' ✨😀');
     
             $this->newLine();
             
             Stache::clear();
-
         } else {
-
             $this->info("Ok, bye then  😥");
 
             $this->newLine();
         }
     }
 
-
-    private function getTableList($tablePrefix)
-    {
-        if (!$tablePrefix) {
-            $tables = DB::select("SHOW TABLES;");
-        } else {
-            $tables = DB::select("SHOW TABLES LIKE '" . $tablePrefix . "\_%';");
-        }
-
-        $tableList = [];
-
-        foreach ($tables as $table) {
-            foreach ($table as $key => $value)
-                $tableList[] = $value;
-        }
-
-        return $tableList;
-    }
-
-    private function getFieldList($tableName)
-    {
-        $fields = DB::select("DESC " . $tableName . ";");
-
-        $optionalFieldList = [];
-
-        $filterFields = ['id','slug','published','published_at','created_at','updated_at','title','name'];
-
-        foreach ($fields as $field) {
-            if (!in_array($field->Field, $filterFields)) {
-                $optionalFieldList[] = $field->Field;
-            }
-        }
-
-        return $optionalFieldList;
-    }
-
-    private function convertRecords($entries, $fieldNames, $collectionName, $imagePrefix, $assetTag)
+    private function convertRecords($entries)
     {
         $bar = $this->output->createProgressBar(count($entries));
-    
+        
         $this->newLine();
-
+        
         $bar->start();
-
+        
+        if ($this->destinationType == 'global') {
+            $global = [];
+        }
+        
         foreach ($entries as $entry) {
-
             $data = new \stdClass();
             
-            // Required fields
-            $data->title = $entry->title ?? $entry->name;
-            $data->updated_at = Carbon::parse($entry->updated_at)->timestamp;
-
-            // Optional fields
-            foreach ($fieldNames as $key => $value) {
-
-                if ($imagePrefix && $assetTag) {
-                    $entryValue = str_replace($imagePrefix, $assetTag, $entry->$value);
-                } else {
-                    $entryValue = $entry->$value;
-                }
-
-                $json = json_decode($entryValue,true);
-
-                if (!is_array($json)) {
-                    $data->$value = $entryValue;
-                } else {
-                    $data->$value = $this->jsonToArray($json, $imagePrefix, $assetTag);
-                }
+            if (isset($entry->file_name) && isset($entry->disk_name)) {
+                $image = file_get_contents('https://cdn.ajarn.com/uploads/public/' . implode('/', array_slice(str_split($entry->disk_name, 3), 0, 3)) . '/' . $entry->disk_name);
+                file_put_contents(public_path('assets/' . $this->destinationName . '/' . $entry->file_name), $image);
             }
 
-            // Create entry
-            $newEntry = Entry::make()
-                ->slug($entry->slug)
-                ->published((boolean) $entry->published)
-                ->collection($collectionName)
-                ->data($data);    
-            $newEntry->date(Carbon::parse($entry->published_at ?? $entry->created_at)->format('Y-m-d'));
-            $newEntry->save();
+            if ($this->destinationType == 'collection') {
+                // Create entry
+                $fields = $this->makeFields($entry);
+
+                if ($this->date) {
+                    $date = Carbon::parse($entry->{$this->date})->format('Y-m-d');
+                } else {
+                    $date = null;
+                }
+            
+                $newEntry = Entry::make()
+                    ->slug($fields['slug'] ?? $fields['title'])
+                    ->date($date)
+                    ->published($fields['published'])
+                    ->collection($this->destinationName)
+                    ->data($fields);
+                $newEntry->save();
+            } elseif ($this->destinationType == 'taxonomy') {
+                // Create term
+                $fields = $this->makeFields($entry);
+                $newTerm = Term::make()
+                    ->slug($fields['slug'] ?? $fields['title'])
+                    ->taxonomy($this->destinationName)
+                    ->data($fields);
+                $newTerm->save();
+            } elseif ($this->destinationType == 'global') {
+                // Create global
+                $fields = $this->makeFields($entry);
+                $fields['type'] = $this->replicatorName;
+                $global[] = $fields;
+            }
 
             $bar->advance();
+        }
+
+        if ($this->destinationType == 'global') {
+            $this->saveGlobal($global);
         }
 
         $bar->finish();
@@ -211,15 +158,61 @@ class DatabaseToStatamic extends Command
         $this->newLine(2);
     }
 
-    private function jsonToArray($json, $imagePrefix, $assetTag)
+    private function makeFields($entry)
+    {
+        foreach ($this->fieldNames as $fieldName => $values) {
+            if ($values['type'] == 'boolean') {
+                $fields[$fieldName] = (boolean)$entry->{$values['from']};
+            } elseif ($values['type'] == 'attachment') {
+                $fields[$fieldName] = 'assets::' . $this->destinationName . '/' . $entry->file_name;
+            } elseif ($values['type'] == 'timestamp') {
+                $fields[$fieldName] = Carbon::parse($entry->{$values['from']})->timestamp;
+            } elseif ($values['type'] == 'relation') {
+                $fields[$fieldName] = DB::table($this->yaml['table']['fields'][$fieldName]['relatedTable'])
+                ->limit(1)->where('id', $entry->{$values['from']})
+                ->get('slug')
+                ->toArray()[0]
+                ->{$values['relatedField']};
+            } elseif ($values['type'] == 'json') {
+                $jsonItems = json_decode($entry->{$values['from']});
+
+                $fields[$fieldName] = [];
+
+                foreach ($jsonItems as $item) {
+                    $fields[$fieldName][] = [
+                        strtolower($item->type) => str_replace(
+                            $values['imageLinks']['prefix'],
+                            $values['imageLinks']['assetTag'],
+                            str_replace(
+                                                            ["\n", "\r"],
+                                                            '',
+                                                            $item->{$values['jsonFields']['content']}
+                                                        )
+                        ),
+                        'type' => strtolower($item->{$values['jsonFields']['type']}),
+                        'enabled' => true
+                    ];
+                }
+            } else {
+                $fields[$fieldName] = $entry->{$values['from']};
+            }
+            
+            if (isset($values['imageLinks'])) {
+                $fields[$fieldName] = str_replace($values['imageLinks']['prefix'], $values['imageLinks']['assetTag'], $fields[$fieldName]);
+            }
+        }
+
+        return $fields;
+    }
+    private function jsonToArray($json)
     {
         $jsonForYaml = [];
-    
+        dd(json_decode($json));
         foreach ($json as $jsonItem) {
             foreach ($jsonItem as $itemKey => $itemValue) {
                 $jsonItem[$itemKey] = str_replace(["\n", "\r"], '', $itemValue);
-                if ($imagePrefix && $assetTag) {
-                    $jsonItem[$itemKey] = str_replace($imagePrefix, $assetTag, $jsonItem[$itemKey]);
+                if ($this->imagePrefix && $this->assetTag) {
+                    $jsonItem[$itemKey] = str_replace($this->imagePrefix, $this->assetTag, $jsonItem[$itemKey]);
                 }
             }
             $jsonItem['enabled'] = true;
@@ -229,4 +222,50 @@ class DatabaseToStatamic extends Command
         return $jsonForYaml;
     }
 
+    private function saveGlobal($global)
+    {
+        $global['sponsor'] = $global;
+
+        $set = GlobalSet::findByHandle($this->destinationName);
+
+        $set = $set->in('default');
+
+        $fields = $set->blueprint()->fields()->addValues($global);
+
+        $values = $fields->process()->values();
+
+        $set->data($values);
+
+        $set->save();
+    }
+
+    private function getEntries()
+    {
+        $attachment = array_filter($this->fieldNames, function ($item) {
+            return isset($item['type']) &&
+                $item['type'] === 'attachment';
+        });
+
+        if (count($attachment) === 1) {
+            foreach ($attachment as $items) {
+                $model = $items['from'];
+            }
+            return DB::table($this->tableName)
+            ->limit($this->tableLimit)
+            ->select($this->tableName.'.*', 'system_files.disk_name', 'system_files.file_name')
+            ->join('system_files', function ($join) use ($model) {
+                $join->on($this->tableName.'.id', '=', 'system_files.attachment_id')
+                     ->where('system_files.attachment_type', '=', $model);
+            })
+            ->orderBy('sort_order', 'ASC')
+            ->orderBy('published_at', 'DESC')
+            ->get();
+        } else {
+            return DB::table($this->tableName)
+            ->limit($this->tableLimit)
+            // ->orderBy('sort_order','ASC')
+            ->orderBy('published_at', 'DESC')
+            ->get();
+        }
+    }
 }
